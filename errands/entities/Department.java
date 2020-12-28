c1a497d03cecec5ec207a08cfb82db886749da25
krisnovitskaya <voitage@yandex.ru>
2020-12-28 19:08:53 +0300

    les03

---
+++ errands/entities/Department.java
@@ -0,0 +1,48 @@
+package entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Department {
+    private String title;
+    private Employee master;
+    private List<Employee> employees;
+    private List<Department> directlySubordinatedDepartments;
+
+    public List<Department> getDirectlySubordinatedDepartments() {
+        return directlySubordinatedDepartments;
+    }
+
+    public void setDirectlySubordinatedDepartments(List<Department> directlySubordinatedDepartments) {
+        this.directlySubordinatedDepartments = directlySubordinatedDepartments;
+    }
+
+    public Department() {
+        employees = new ArrayList<>();
+        directlySubordinatedDepartments = new ArrayList<>();
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Employee getMaster() {
+        return master;
+    }
+
+    public void setMaster(Employee master) {
+        this.master = master;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+}
